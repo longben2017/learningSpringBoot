b9a64be38051b90c3151235a2c2098e3524c0877
longgq <longgq@onecloud.cn>
2017-12-13 14:28:33 +0800

    spring-data-jpa

---
+++ chapter5/src/main/java/com/longgq/domain/UserRepository.java
@@ -0,0 +1,18 @@
+package com.longgq.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+/**
+ * Created by clouder on 17-12-13.
+ */
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByName(String name);
+
+    User findByNameAndAge(String name, Integer age);
+
+    @Query("from User u where u.name=:name")
+    User findUser(@Param("name") String name);
+
+}
